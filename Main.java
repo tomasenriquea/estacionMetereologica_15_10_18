88684df26f8755035e90c53108369f1679436576
Tomas Enrique <tomasenriquea80@gmail.com>
2018-10-15 17:15:52 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+package uF1.ejercicios.estacionMetereologica_15_10_18;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		
+		
+
+	}
+
+}
---
+++ Meteo.java
@@ -0,0 +1,5 @@
+package uF1.ejercicios.estacionMetereologica_15_10_18;
+
+public class Meteo {
+
+}
